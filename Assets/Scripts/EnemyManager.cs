57fe3db3a1686db5314150f6d449da742b00373c
Ignacio Baptista [PC] <ibaptista@frba.utn.edu.ar>
2021-12-11 18:49:35 -0300

    Agregada animacion al mostrar hits

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -1,19 +1,18 @@
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Assertions;
 
 public class EnemyManager : MonoBehaviour
 {
-    // the life of the enemy
-    private float life = 4;
-    // Shooting range
-    private const float shootingRange = 1.5f;
+    private const float shootingRange = 1.5f, totalCooldown = 2f, animationTime = 0.1f, showTime = 0.8f;
     
-    private const float totalCooldown = 2f;
-    private float cooldownLeft = 2f;
+    private float cooldownLeft = 2f, life = 4;
     private bool isInCooldown;
 
     private TMP_Text damageText;
+
+    private bool destroyed;
 
     private void Awake()
     {
@@ -26,9 +25,7 @@ public class EnemyManager : MonoBehaviour
     {
         damageText.enabled = false;
     }
-
-    private const float showTime = 0.7f;
-
+    
     public void takeDamage(float damageTaken)
     {
         // lose life
@@ -36,28 +33,49 @@ public class EnemyManager : MonoBehaviour
         
         if (life <= 0)
         {
+            Debug.Log("Me muero pq no tengo mas vida");
+            destroyed = true;
             Destroy(gameObject);
             return;
         }
         
+        Debug.Log("Ahr q no mori");
         showDamage(damageTaken);
     }
 
     private void showDamage(float damageTaken)
     {
+        if (destroyed)
+            return;
+        
         damageText.text = "-" + damageTaken;
         damageText.enabled = true;
-        
-        LeanTween.value(0, showTime, showTime).setOnComplete(hideDamage);
+
+        // En 0.7 segundos muestro el texto
+        damageText.gameObject.transform.DOLocalMoveY(1.17f, animationTime).onComplete += hideDamage;
     }
 
     private void hideDamage()
     {
-        damageText.enabled = false;
+        if (destroyed)
+            return;
+        
+        // Espero 0.8f segundos para que se oculte, y lo oculto en el mismo tiempo que se motro
+        damageText.gameObject.transform.DOLocalMoveY(0.78f, animationTime).SetDelay(showTime).onComplete += () =>
+        {
+            if (destroyed)
+                return;
+            
+            // Se desactiva el objeto
+            damageText.enabled = false;
+        };
     }
 
     private void Update()
     {
+        if (destroyed)
+            return;
+        
         if (isInCooldown)
         {
             // reduce cooldown
--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -2,57 +2,59 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    private Transform enemy;
+    private float totalCooldown, currentCoodlown;
     
-    private Transform player;
-    private Transform parent;
+    private const float spawningDistance = 41;
 
-    // Start is called before the first frame update
+    private Transform enemy, player, parent;
+    
+    private readonly Vector3 spawnLimit1 = new Vector3(-100, 2, 100), spawnLimit2 = new Vector3(100, 2, -46);
+
+    public float randomMin = 4, randomMax = 8;
+
     void Start()
     {
         // load enemy from Prefabs/Enemy
         enemy = (Resources.Load("Prefabs/Enemy") as GameObject)?.transform;
         player = GameObject.Find("First Person Player").GetComponent<Transform>();
         parent = GameObject.Find("Enemies").GetComponent<Transform>();
+        
+        generateRandomCooldown();
     }
 
-    private float currentCoodlown;
-    private const float totalCooldown = 0.1f;
-    
-    private readonly Vector3 spawnLimit1 = new Vector3(-100,2,100), 
-                             spawnLimit2 = new Vector3(100,2,-46);
+    private void generateRandomCooldown()
+    {
+        totalCooldown = Random.Range(randomMin, randomMax);
+        currentCoodlown = totalCooldown;
+    }
 
-    // Spawning distance 75
-    private const float spawningDistance = 75;
-    
-    // Update is called once per frame
     void Update()
     {
-        // create an enemy every "totalCooldown" seconds
-        if (currentCoodlown <= 0)
-        {
-            bool generacionCorrecta = false;
-            Vector3 spawnPosition = new Vector3(0,0,0);
-            
-            while (!generacionCorrecta)
-            {
-                // Generate a random position based on spawnLimit1 and spawnLimit2
-                spawnPosition = new Vector3(Random.Range(spawnLimit1.x, spawnLimit2.x), Random.Range(spawnLimit1.y, spawnLimit2.y), Random.Range(spawnLimit1.z, spawnLimit2.z));
-                
-                // Check if spawn is not too near to player
-                if (Vector3.Distance(player.position, spawnPosition) > spawningDistance)
-                    generacionCorrecta = true;
-            }
-
-            // Create an enemy on the spawn position
-            Transform myEnemy = Instantiate(enemy, spawnPosition, Quaternion.identity);
-            myEnemy.parent = parent;
-            
-            currentCoodlown = totalCooldown;
-        }
-        else
+        // Reduce cooldown if its not finished
+        if (currentCoodlown > 0)
         {
             currentCoodlown -= Time.deltaTime;
+            return;
         }
+        
+        bool generacionCorrecta = false;
+        Vector3 spawnPosition = new Vector3(0,0,0);
+        
+        while (!generacionCorrecta)
+        {
+            // Generate a random position based on spawnLimit1 and spawnLimit2
+            spawnPosition = new Vector3(Random.Range(spawnLimit1.x, spawnLimit2.x), Random.Range(spawnLimit1.y, spawnLimit2.y), Random.Range(spawnLimit1.z, spawnLimit2.z));
+            
+            // Check if spawn is not too near to player
+            if (Vector3.Distance(player.position, spawnPosition) > spawningDistance)
+                generacionCorrecta = true;
+        }
+
+        // Create an enemy on the spawn position
+        Transform myEnemy = Instantiate(enemy, spawnPosition, Quaternion.identity);
+        myEnemy.parent = parent;
+
+        generateRandomCooldown();
+        
     }
 }
